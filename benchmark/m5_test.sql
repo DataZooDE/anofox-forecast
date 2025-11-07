211972cbd1ce09435f919a33db2af7a1678976a6
Simon Müller <simon.mueller@muon-stat.com>
2025-11-07 17:07:32 +0100

    refactor: Update SQL queries in m5_test.sql for consistency and clarity
    Changes include:
    - Renamed columns in the SELECT statements for better readability (`timestamp` to `ds`, `demand` to `y`).
    - Adjusted the forecast function calls to match the new column names.
    - Simplified the evaluation results aggregation process by summarizing model performance.
    - Removed deprecated ETS forecast section and streamlined the evaluation results table.
    
    These modifications enhance the clarity of the SQL script and ensure consistency across the data processing steps.

--- benchmark/m5_test.sql
+++ benchmark/m5_test.sql
@@ -5,7 +5,7 @@ INSTALL httpfs;
 LOAD 'httpfs';
 
 CREATE OR REPLACE TABLE m5 AS 
-SELECT item_id, CAST(timestamp AS TIMESTAMP) AS timestamp, demand FROM 'https://m5-benchmarks.s3.amazonaws.com/data/train/target.parquet'
+SELECT item_id, CAST(timestamp AS TIMESTAMP) AS ds, demand AS y FROM 'https://m5-benchmarks.s3.amazonaws.com/data/train/target.parquet'
 ORDER BY item_id, timestamp;
 
 
@@ -69,76 +69,73 @@ SELECT
 
 -- Create train / test split for the last 12 months
 CREATE OR REPLACE TABLE m5_train AS
-SELECT * FROM m5 WHERE timestamp < DATE '2016-04-25';
+SELECT * FROM m5 WHERE ds < DATE '2016-04-25';
 
 CREATE OR REPLACE TABLE m5_test AS
-SELECT * FROM m5 WHERE timestamp >= DATE '2016-04-25';
+SELECT * FROM m5 WHERE ds >= DATE '2016-04-25';
 
 
 -- Perform baseline forecast and evaluate performance
 CREATE OR REPLACE TABLE forecast_results AS (
     SELECT *
-    FROM TS_FORECAST_BY('m5_train', [item_id, product_id], timestamp, demand, 'SeasonalNaive', 28, {'seasonal_period': 7})
+    FROM TS_FORECAST_BY('m5_train', item_id, ds, y, 'SeasonalNaive', 28, {'seasonal_period': 7})
     UNION ALL
     SELECT *
-    FROM TS_FORECAST_BY('m5_train', [item_id, product_id], timestamp, demand, 'Theta', 28, {'confidence_level': 0.95})
+    FROM TS_FORECAST_BY('m5_train', item_id, ds, y, 'Theta', 28, {'confidence_level': 0.95})
     UNION ALL
     SELECT *
-    FROM TS_FORECAST_BY('m5_train', [item_id, product_id], timestamp, demand, 'AutoETS', 28, {'seasonal_period': 7})
-    UNION ALL
-    SELECT *
-    FROM TS_FORECAST_BY('m5_train', [item_id, product_id], timestamp, demand, 'ARIMA', 28, {'p': 2, 'd': 1, 'q': 2, 'P': 1, 'D': 1, 'Q': 1, 's': 7})
+    FROM TS_FORECAST_BY('m5_train', item_id, ds, y, 'AutoARIMA', 28, {'seasonal_period': 7})
 );
-
 
 
 -- MAE and Bias of Naive Forecast
 CREATE OR REPLACE TABLE evaluation_results AS (
 SELECT 
     item_id,
-    TS_MAE(LIST(demand), LIST(point_forecast)) AS naive_mae,
-    TS_BIAS(LIST(demand), LIST(point_forecast)) AS naive_bias
+    model_name,
+    TS_MAE(LIST(y), LIST(point_forecast)) AS mae,
+    TS_BIAS(LIST(y), LIST(point_forecast)) AS bias
 FROM (
     -- Join Naive Forecast with Test Data
     SELECT 
         m.item_id,
-        m.timestamp,
-        m.demand,
+        m.ds,
+        m.y,
+        n.model_name,
         n.point_forecast
-    FROM naive_forecast n
-    JOIN m5_test m ON n.item_id = m.item_id AND n.forecast_timestamp = m.timestamp
+    FROM forecast_results n
+    JOIN m5_test m ON n.item_id = m.item_id AND n.date_col = m.ds
 )
-GROUP BY item_id
+GROUP BY item_id, model_name
 );
 
-
--- ETS Forecast
-CREATE OR REPLACE TABLE ets_forecast AS (
-    SELECT *
-    FROM TS_FORECAST_BY('m5_train', item_id, timestamp, demand, 'ETS', 28, MAP{'seasonal_period': 7, 'error_type': 0, 'trend_type': 1, 'season_type': 1})
-);
+-- Summarise evaluation results by model
+SELECT model_name, AVG(mae) AS avg_mae, AVG(bias) AS avg_bias FROM evaluation_results GROUP BY model_name ORDER BY avg_mae;
 
 
--- Calculate MAE and Bias of Theta Forecast and append to evaluation_results
-CREATE OR REPLACE TABLE evaluation_results AS (
-    SELECT * FROM evaluation_results e
-    LEFT JOIN (
-        SELECT 
-            item_id,
-            TS_MAE(LIST(demand), LIST(point_forecast)) AS ets_mae,
-            TS_BIAS(LIST(demand), LIST(point_forecast)) AS ets_bias
-        FROM (
-            -- Join ETS Forecast with Test Data
-            SELECT 
-                m.item_id,
-                m.timestamp,
-                m.demand,
-                n.point_forecast
-            FROM ets_forecast n
-            JOIN m5_test m ON n.item_id = m.item_id AND n.forecast_timestamp = m.timestamp
-            )
-            GROUP BY item_id
-    ) t ON e.item_id = t.item_id
-);
-
-SELECT * FROM evaluation_results;
+-- Create Aggregated Forecasts for all models and evaluate performance
+WITH aggregated_forecasts AS (
+    SELECT 
+        model_name,
+        date_col AS ds,
+        sum(point_forecast) AS point_forecast
+    FROM forecast_results
+    GROUP BY model_name, date_col
+),
+aggregated_test AS (
+    SELECT 
+        ds,
+        sum(y) AS y
+    FROM m5_test
+    GROUP BY ds
+),
+evaluation_results AS (
+    SELECT 
+        model_name,
+        TS_MAE(LIST(y), LIST(point_forecast)) AS mae,
+        TS_BIAS(LIST(y), LIST(point_forecast)) AS bias
+    FROM aggregated_forecasts
+    JOIN aggregated_test ON aggregated_forecasts.ds = aggregated_test.ds
+    GROUP BY model_name
+)
+SELECT model_name, AVG(mae) AS avg_mae, AVG(bias) AS avg_bias FROM evaluation_results GROUP BY model_name;
